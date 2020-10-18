eed9ce942fb4e2527fe1e2d3ef849d7b09b813dc
Barzov-Pavel <barzov_pavel@mail.ru>
2020-10-18 15:33:32 +0300

    Played with get request param

--- src/main/java/by/barzov/springmvc/controller/FirstController.java
+++ src/main/java/by/barzov/springmvc/controller/FirstController.java
@@ -2,12 +2,17 @@ package by.barzov.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String sayHello() {
+    public String sayHello(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello " + name + " " + surname);
         return "/first/hello";
     }
 
